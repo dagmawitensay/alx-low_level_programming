a269df5154b78736b9fc2b1286ee8c1c07fcd1f3
dagiTensay <dagitensay@gmail.com>
2022-12-08 10:04:57 -0800

    fprintf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,20 @@
 #include <unistd.h>
 
 /**
- * main - entry point of the program
- * Description: The program prints error
- * message to the console
- * Return:1 alwats returns 1
- */
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 1 (Success)
+ *
+ *     */
+
 int main(void)
 {
-	write(2, "and that piece of art is useful\" -
-			Dora Korpar, 2015-10-19\n",58);
-	return (1);
+
+		char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+		write(2, str1, 59);
+		return (1);
 }
