c253ac1fc8b378c752e1202c52cd6ddb698d888d
dagiTensay <dagitensay@gmail.com>
2022-12-08 08:38:07 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+/*
+ * main - entry point prints with printf
+ * Return: 0 always returns zero
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
