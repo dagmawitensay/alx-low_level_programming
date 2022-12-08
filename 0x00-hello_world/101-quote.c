9426ea3d3f0f3bd25c917887abd24c2e09493ca5
dagiTensay <dagitensay@gmail.com>
2022-12-08 10:11:28 -0800

    write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,17 @@
 #include <unistd.h>
 
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 1 (Success)
- *
- *     */
+ * main - Entry point
+ * Description: prints the given text
+ * with write function
+ * Return: Always 1 (Success)
+ */
 
 int main(void)
 {
 
-		char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+		char str1[] = "and that piece of art is
+			useful\" - Dora Korpar, 2015-10-19\n";
 
 		write(2, str1, 59);
 		return (1);
