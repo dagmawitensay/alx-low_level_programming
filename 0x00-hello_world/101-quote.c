a76ce44913e5d335c068ca1f5dfdc26ea0590fc0
dagiTensay <dagitensay@gmail.com>
2022-12-08 10:26:57 -0800

    write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,17 +2,21 @@
 
 /**
  * main - Entry point
- * Description: prints the given text
- * with write function
+ * Description : printing with write function
  * Return: Always 1 (Success)
  */
 
 int main(void)
+
 {
 
-		char str1[] = "and that piece of art is
-			useful\" - Dora Korpar, 2015-10-19\n";
+		char str1[] = "and that price of art is useful\" - Dora
+			Korpar, 2015-10-19\n";
 
-		write(2, str1, 59);
-		return (1);
+
+
+			write(2, str1, 59);
+
+				return (1);
+
 }
