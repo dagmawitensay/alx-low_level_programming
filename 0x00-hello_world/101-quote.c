62e2693d08f335d6d0964870bdc546cfa6ce46b9
dagiTensay <dagitensay@gmail.com>
2022-12-08 09:54:48 -0800

    fprintf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ * Description: The program prints error
+ * message to the console
+ * Return:1 alwats returns 1
+ */
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" -
+			Dora Korpar, 2015-10-19\n");
+	return (1);
+}
