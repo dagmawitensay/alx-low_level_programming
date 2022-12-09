9e62771c1de29bf966d5c94db22177125e8d8f96
dagiTensay <dagitensay@gmail.com>
2022-12-09 08:51:02 -0800

    smallCAPITAL

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - a function that prints
+ * the alphabet in uppercase and in
+ * lowercase
+ * Return: 0 always returns zero
+ */
+
+int main(void)
+{
+	char c = 'a';
+	char d = 'A';
+	short int i = 0;
+	short int j = 0;
+
+	for (i = 0 ; i < 26; i++)
+	{
+		putchar(c);
+		c += 1;
+	}
+
+	for (j = 0 ; j < 26; i++)
+	{
+		putchar(d);
+		d += 1;
+	}
+	putchar('\n');
+}
