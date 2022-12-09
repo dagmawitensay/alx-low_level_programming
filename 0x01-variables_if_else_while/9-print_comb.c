7db721d96be6e1ab5e2ad434941f8c9c5d851024
dagiTensay <dagitensay@gmail.com>
2022-12-09 10:35:52 -0800

    Task 9"

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints digits in asending
+ * order
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		putchar(num);
+		putchar(',');
+		num++;
+	}
+	putchar('$');
+	putchar('\n');
+	return (0);
+}
