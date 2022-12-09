93b78e43084e5fe4a26148028661fac66579a90e
dagiTensay <dagitensay@gmail.com>
2022-12-09 08:11:53 -0800

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,14 +16,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 0)
-	{
-		last_digit = (n % 10) - 10;
-	}
-	else
-	{
-		last_digit = (n % 10);
-	}
+	last_digit = n % 10
 	if (last_digit > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
@@ -34,10 +27,10 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+		printf
+			("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 	}
 	return (0);
-
 }
 
 
