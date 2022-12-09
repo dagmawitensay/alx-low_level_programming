77d8d68da4c585d99d5bfd87a33a3f80b4d63334
dagiTensay <dagitensay@gmail.com>
2022-12-09 10:41:36 -0800

    Task 9"

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,10 +13,18 @@ int main(void)
 
 	while (num <= 57)
 	{
+		if (num != 57)
+		{
 		putchar(num);
 		putchar(',');
 		putchar(' ');
 		num++;
+		}
+		else
+		{
+		putchar(num);
+		num++;
+		}
 	}
 	putchar('\n');
 	return (0);
