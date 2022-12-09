0ba83a3a127cc39eba35998ab1ffefc71b6192f0
dagiTensay <dagitensay@gmail.com>
2022-12-09 08:23:24 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - a function that prints
+ * the english alphabet
+ * Return: 0 always returns zero
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c != 'z')
+	{
+		putchar(c);
+		c += 1;
+	}
+	putchar('\n');
+}
