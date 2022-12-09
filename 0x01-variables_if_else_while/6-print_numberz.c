cf7f30f48fdf633a90c59d46c95c93524b31925f
dagiTensay <dagitensay@gmail.com>
2022-12-09 10:05:45 -0800

    only put char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - a function that prints digits
+ * from zero to nine
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned int num = 0;
+
+	while (num <= 9)
+	{
+		putchar(i);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
