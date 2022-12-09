c0f7ebdc28a70a7d13de207d2ba42e25fd9aed1c
dagiTensay <dagitensay@gmail.com>
2022-12-09 12:07:48 -0800

    Combination of digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints number combinations
+ * Return: 0 sucess
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		for (j = '0' ; j <= '9' ; j++)
+		{
+			if (i < j and i != '9')
+			{
+				putchar(i);
+				putchar(j);
+				if (i < '9' | i < '8')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+i	return (0);
+}
