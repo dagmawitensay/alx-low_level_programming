123adf50a3a1adfecc09ed5bd068f1ae9ee60dae
dagiTensay <dagitensay@gmail.com>
2022-12-13 03:58:02 -0800

    alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <main.h>
+
+/**
+ * print_alphabet_x10 - prints the alphabet
+ * in lowercase 10 times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c = 97;
+	int i = 0;
+
+	while (i < 10)
+	{
+		while (c < 123)
+		{
+			_putchar(c);
+			c++;
+		}
+		i++;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,9 @@
 /**
  * _putchar - writes putchar to the stdout
  * print_alphabet - prints the alphabet
+ * print_alphabet_x10 - prints the alphabet in lowecase 10 times
  */
 
 int _putchar(char);
 void print_alphabet(void);
+void print_alphabet_x10(void);
