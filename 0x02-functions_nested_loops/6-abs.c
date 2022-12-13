6e41b703fc4ffbcfa0a8ca6b6df985ffc1f234ca
dagiTensay <dagitensay@gmail.com>
2022-12-13 05:06:14 -0800

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - returns the absolute value of
+ * a given number
+ * @c: the number to be evaluated
+ * Return: the absolute value of the number
+ */
+
+
+
+
+int _abs(int c)
+{
+	if (c >= 0)
+	{
+		return (c);
+	}
+	return (-1 * c);
+}
