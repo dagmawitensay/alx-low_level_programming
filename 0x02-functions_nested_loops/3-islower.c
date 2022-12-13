e34223379bcdad0f6919cb99bb20dd61175cb519
dagiTensay <dagitensay@gmail.com>
2022-12-13 04:36:59 -0800

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "mian.h"
+
+/**
+ * _islower - check if a letter is
+ * in lower case or not
+ * @c: character to be checked
+ * Return: 1 if lower case
+ * and 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 97 and c < 123)
+	{
+		return (0);
+	}
+	return (1);
+}
