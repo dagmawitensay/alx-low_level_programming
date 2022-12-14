ade6b96b52164deca9853ca979da6510fc41666d
dagiTensay <dagitensay@gmail.com>
2022-12-14 05:25:50 -0800

    n times tabe

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,39 @@
+#include "mian.h"
+/**
+ * print_times_table - prints n by n mulipliction table
+ * @n: the given number
+ * Return: void not return
+ */
+
+
+
+void print_times_table(int n)
+{
+	if (n > 0 && n <= 15)
+	{
+		int prod;
+		int i = 0;
+
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(48);
+			_putchar(',');
+			_putchar(' ');
+			for (j = 0; j <= n; i++)
+			{
+				prod = j * i;
+				if (prod < 10)
+				{
+					_putchar(' ');
+					_putchar(prod + 48);
+				} else
+				{
+					_putchar(prod / 10 + 48);
+					_putchar(prod % 10 + 48);
+				}
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
