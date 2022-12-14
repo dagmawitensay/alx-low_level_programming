067f74a66c1fd81546220e797589742daa2db37b
dagiTensay <dagitensay@gmail.com>
2022-12-14 13:28:45 -0800

    multiples sum

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * multiples_sum - prints the sum of all multiples
+ * of 3 and 5 below 1023
+ * Return: void no return
+ */
+
+void multiples_sum(void)
+{
+	int i = 0;
+	int total = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			total += i;
+		}
+	}
+	printf("%d", total);
+}
