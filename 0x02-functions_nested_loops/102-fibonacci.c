6cfd3cbf73350b49969816c7a202470a4ccf0b52
dagiTensay <dagitensay@gmail.com>
2022-12-14 13:50:16 -0800

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * main - prints fibonacci numbers upto 50
+ * Return: 0 Suecess
+ */
+
+int main(void)
+{
+	int a = 1;
+	int b = 2;
+	int c;
+	int j;
+
+	_putchar(a + 48);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(b + 48);
+	_putchar(',');
+	_putchar(' ');
+	for (j = 2; j < 50; j++)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		if (c < 10)
+		{
+			_putchar(c + 48);
+		} else
+		{
+			_putchar(c / 10 + 48);
+			_putchar(c % 10 + 48);
+		}
+		if (j != 49)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		return (0);
+	}
+	_putchar('\n');
+}
+
