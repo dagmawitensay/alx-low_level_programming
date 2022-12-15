b2b2afa7ddde44a2528939d009660b84c3c8310d
dagiTensay <dagitensay@gmail.com>
2022-12-15 10:44:17 -0800

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - returns the value of the multiplicatiojn of two
+ * given numbers
+ * @a: the first number
+ * @b: the second number
+ * Return: a * b
+ */
+
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
