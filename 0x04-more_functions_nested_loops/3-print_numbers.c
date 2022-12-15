31448afddf77edb186e5eb9c08badab54b6cd50e
dagiTensay <dagitensay@gmail.com>
2022-12-15 10:57:44 -0800

    Prints digits

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * print_numbers - prints the digit form 0 to 9
+ * Return: no return
+ */
+
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		_puchar(i + 48);
+	}
+	_putchar('\n');
+}
