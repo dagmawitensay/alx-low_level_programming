c6d13260aeb819f3a73b4700a7133a72a43ee953
dagiTensay <dagitensay@gmail.com>
2022-12-15 12:14:35 -0800

    Task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *more_numbers - prints the digits from 0 to 14 ten times
+ *Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			_putchar(j + 48);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
