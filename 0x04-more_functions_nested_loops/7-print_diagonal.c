885f453e30c7268ed1c3ba3ec7a2af51c7e44a52
dagiTensay <dagitensay@gmail.com>
2022-12-15 13:09:16 -0800

    Diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,7 +8,7 @@
 
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	if (n <= 0)
 	{
@@ -16,12 +16,14 @@ void print_diagonal(int n)
 	}
 	else
 	{
-		_putchar(92);
-		for (i = 1; i < n; i++)
+		for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
 			_putchar(92);
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
