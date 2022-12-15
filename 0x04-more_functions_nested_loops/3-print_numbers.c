7b8abe4e5fbf38051f101d5542fbcb791e8f4bb1
dagiTensay <dagitensay@gmail.com>
2022-12-15 11:19:05 -0800

    print digits

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "_putchar.c"
 
 
 /**
@@ -11,9 +12,9 @@ void print_numbers(void)
 {
 	int i = 0;
 
-	for (i = 48; i <= 57; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		_putchar(i);
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
