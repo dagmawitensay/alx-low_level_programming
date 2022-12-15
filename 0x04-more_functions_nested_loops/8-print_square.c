c1990be6f01434703019313c29f6e8f5d9ace2cb
dagiTensay <dagitensay@gmail.com>
2022-12-15 13:17:29 -0800

    Square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "mian.h"
+
+/**
+ * print_square - prints square using # character
+ * @size: square size
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+	for (i = 0; i < size; i++)
+	{
+	for (j = 0; j < size; j++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
+	_putchar('\n');
+	}
+	else
+	{
+	_putchar('\n');
+	}
+}
