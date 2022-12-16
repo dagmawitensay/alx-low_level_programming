305eba95f0bddc6ab3b0a6df9b9dfff26594cf25
dagiTensay <dagitensay@gmail.com>
2022-12-16 12:58:12 -0800

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,15 +13,11 @@ void more_numbers(void)
 	{
 		for (j = 0; j < 15; j++)
 		{
-			if ( j <= 9)
-			{
-				_putchar(j + 48);
-			}
-			else
+			if (j > 9)
 			{
 				_putchar(j / 10 + 48);
-				_putchar(j % 10 + 48);
 			}
+			_putchar(j % 10 + 48);
 		}
 		_putchar('\n');
 	}
