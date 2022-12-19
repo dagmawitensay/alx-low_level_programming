7a6a59833b82f5170efa3ff734330a48f2a76128
dagiTensay <dagitensay@gmail.com>
2022-12-19 13:59:16 -0800

    n elemnts of the array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_array - prints n elements of the array
@@ -13,7 +14,7 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		_putchar(a[i]);
+		printf("%d", a[i]);
 		if (i != n - 1)
 		{
 			_putchar(',');
