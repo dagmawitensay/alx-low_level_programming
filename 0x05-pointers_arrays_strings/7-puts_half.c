8cdc5d82ee400decc40367522b56771cc3be35f9
dagiTensay <dagitensay@gmail.com>
2022-12-19 13:46:10 -0800

    The second half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - only prints the second half of
+ * a given string
+ * @str: string to be printed
+ * Return:noting
+ */
+
+
+void puts_half(char *str)
+{
+	int len = strlen(str);
+	int i, j;
+
+	if (len % 2 == 0)
+	{
+		j = len / 2;
+	} else
+	{
+		j = (len - 1) / 2;
+	}
+
+	for (i = j; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
