a09258a3b5ccc4fc7192e1c1cc8d8672902882d1
dagiTensay <dagitensay@gmail.com>
2022-12-19 11:50:48 -0800

    print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+
+/**
+ * print_rev - print the string in reverse
+ * @s: string to be printed
+ * Return: noting
+ */
+
+void print_rev(char *s)
+{
+	int i = strlen(s);
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
