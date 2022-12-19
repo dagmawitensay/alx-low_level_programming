4da010b29200f2efcec9a5e7f76c3ddc4154ae63
dagiTensay <dagitensay@gmail.com>
2022-12-19 09:30:24 -0800

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - accepts an int parametr and changes the
+ * value it points to 98
+ * @n: an int pointer
+ * Return: noting
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
