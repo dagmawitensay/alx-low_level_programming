ba5b6a6487abe8fd2de45cfe50b6b0b4bfe677ae
dagiTensay <dagitensay@gmail.com>
2022-12-19 11:38:30 -0800

    print to the stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to the stdout
+ * @str: a string to be printed
+ * Return: noting
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+	}
+}
