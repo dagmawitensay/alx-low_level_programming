84722fec57cd33a292e620f0fa0af2511fae5d56
dagiTensay <dagitensay@gmail.com>
2023-01-02 11:51:33 -0800

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strpbrk - returns a pointer to the first matching
+ * character is s with any of the characters in accept
+ * @s: string to be scanned
+ * @accept :string cotaining the charactes to match
+ */
+
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i = 0;
+	unsigned int j;
+
+	while (*(s + i) != '\0')
+	{
+		j = 0;
+
+		while (*(accept + j) != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+			j++;
+		}
+		s++;
+	}
+	return (0);
+}
