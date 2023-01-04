11c436e813ab652ed2627c9f7161f4877498efdc
dagiTensay <dagitensay@gmail.com>
2023-01-04 11:04:31 -0800

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - main function
+ * @n: number to be evaluated
+ * Return : -1 if no natural square root
+ * or value of squre root
+ */
+
+
+int _sqrt_recursion(int n)
+{
+	_sqrt(n, 1);
+}
+
+/**
+ * _sqrt - evaluator function
+ * @n :number to be evauated
+ * @i: possible root
+ * Return: root
+ */
+
+int _sqrt(int n, int i)
+{
+	if (n < 0 || i * i > n)
+		return (-1);
+	else if (i * i == n)
+		return (i);
+	_sqrt(n, i + 1);
+}
