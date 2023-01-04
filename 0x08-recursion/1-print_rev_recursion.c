3bdd1a82255f2d69f31ea52849c6989c01ff4c56
dagiTensay <dagitensay@gmail.com>
2023-01-04 05:27:07 -0800

    print reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+
+void _rev_helper(char *s, int i)
+{
+	if (i >= 0)
+	{
+		_putchar(*(s + i));
+		_rev_helper(s, i - 1);
+	}else
+	{
+		return;
+	}
+
+}
+
+/**
+ * _print_rev_recursion - function that prints a string in reverse
+ * @s: string to be printed
+ */
+
+void _print_rev_recursion(char *s)
+{
+	int n = strlen(s);
+
+	_rev_helper(s, n);
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,1 +1,2 @@
 1). A function that prints a string, followed by a new line.
+2). A function that prints a string in reverse.
