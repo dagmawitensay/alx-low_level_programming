da8490042ccfc79d7340a97cbdff9c10ce784cab
dagiTensay <dagitensay@gmail.com>
2023-01-09 12:19:34 -0800

    name of the program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - funct that prints name of the program
+ * @argc: number of arguments
+ * @argv: pointer to an array of pointers
+ * Return: 0 sucess
+ */
+
+int main(int __attribute__ ((unused)) argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H 
+int main(int argc, char **argv);
+int __attribute__((unused)) argc;
+#endif /*MAIN_H*/
