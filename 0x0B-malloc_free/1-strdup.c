cb8a7d21858d3d2caabd63056602cac63224f755
dagiTensay <dagitensay@gmail.com>
2023-01-10 14:12:35 -0800

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - function that copies the the content of given
+ * array into new array
+ * @str: array to be copied
+ * Return: pointer to the new array
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int len = strlen(str);
+	unsigned int i;
+	char *s;
+
+	s = malloc(len * sizeof(char));
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0;  i < len; i++)
+	{
+		s[i] = str[i];
+	}
+	return (s);
+}
