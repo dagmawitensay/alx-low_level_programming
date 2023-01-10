6cb497765872d8e91b3664a42be1c339915f364e
dagiTensay <dagitensay@gmail.com>
2023-01-10 14:32:54 -0800

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,6 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+/**
+ * _strlen - returns the length of the string
+ * @s: input string to count
+ * Description: returns the length of a given string
+ * Return: length of string as int
+ **/
+int _strlen(char *s)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i + 1);
+}
+
+
 /**
  * _strdup - function that copies the the content of given
  * array into new array
@@ -11,14 +28,13 @@
 
 char *_strdup(char *str)
 {
-	unsigned int len = strlen(str);
 	unsigned int i;
 	char *s;
 
 	if (str == NULL)
 		return (NULL);
 
-	s = malloc(len * sizeof(char));
+	s = (char *) malloc(_strlen(str) * sizeof(char));
 
 	if (s == NULL)
 		return (NULL);
@@ -27,7 +43,6 @@ char *_strdup(char *str)
 	{
 		s[i] = str[i];
 	}
-	s[i] = str[i];
 
 	return (s);
 }
