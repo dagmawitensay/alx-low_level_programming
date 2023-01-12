c8fbdcdb43dfb32339c513e264f8c74c2fa45bbc
dagiTensay <dagitensay@gmail.com>
2023-01-12 15:10:48 -0800

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * malloc_checked - function that allocates memory space using malloc
+ * @b: size of the memory space
+ * Return:noting
+ */
+
+void *malloc_checked(unsigned int b)
+{
+
+	void *s = malloc(b);
+	
+	if (s == NULL)
+		exit(98);
+	return (s);
+}
