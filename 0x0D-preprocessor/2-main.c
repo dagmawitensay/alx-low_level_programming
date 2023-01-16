b3ae105b697ebc8a0baff862a9329c7ce65cbc00
dagiTensay <dagitensay@gmail.com>
2023-01-16 13:42:05 -0800

    location of the file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - function that prints the name of the
+ * file it was compiled on
+ * Return: noting
+ */
+
+int main(void)
+{
+	prinf("%s\n", __FILE__);
+}
