01fe46f3aa4d6f5bea6a6fcbe3a8a3ea61a1f763
dagiTensay <dagitensay@gmail.com>
2023-01-17 14:51:02 -0800

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct defining dog properties
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: ownert of the dog
+ */
+
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
