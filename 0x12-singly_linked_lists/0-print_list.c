f7de6851dd08f10798bcac40153cbc731583be73
dagiTensay <dagitensay@gmail.com>
2023-01-26 03:07:57 -0800

    print elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_list - function that prints elemets of the linke
+ * list and returns the number of elements
+ * @h: given linked list
+ * Return: the number of elements in the list
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
