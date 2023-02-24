85343151761392874c49edba58089dcff7f55b05
dagiTensay <dagitensay@gmail.com>
2023-02-24 02:34:42 -0800

    binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - function that converts a given binary string to
+ * unsinged int and returns it
+ * @b: pointer to the string to be converted
+ * Return: converted unsinged int
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int result = 0;
+
+	if (!b)
+		return (0);
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		result  = 2 * result + (b[i] - '0');
+	}
+	return (result);
+}
