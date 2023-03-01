ade3cc8088d9805d3c568420e489800d39671f86
dagiTensay <dagitensay@gmail.com>
2023-02-28 20:54:31 -0800

    before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void before_main(void) __attribute__((constructor));
+
+/**
+ * before_main - function that is exectued before the main function
+ * Return: noting
+ */
+
+void before_main(void)
+{
+	printf(
+		"You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
+		);
+}
