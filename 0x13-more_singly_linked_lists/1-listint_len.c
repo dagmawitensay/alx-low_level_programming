15231243d47436d411e7b847181989b8606024ec
dagiTensay <dagitensay@gmai.com>
2023-03-04 06:18:53 -0800

    find length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#inclue "lists.h"
+
+/**
+ * listint_len - function that returns the number of elements
+ * of a list
+ * @h: pointer to the list
+ * Return: number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t n;
+
+	for (n = 0; h != NULL; n++)
+	{
+		h = h->next;
+	}
+
+	return (n);
+}
