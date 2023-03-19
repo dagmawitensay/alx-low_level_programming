8fd524c5134010f2ec2e81fc271f30d732d98e0f
dagiTensay <dagitensay@gmai.com>
2023-03-19 01:11:58 -0700

    delete at index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -16,33 +16,30 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (*head == NULL)
 		return (-1);
-
 	while (start->next != NULL)
 	{
 		len++;
 		start = start->next;
 	}
 	len++;
-
 	if (index > len)
 		return (-1);
-
 	if (index == 0)
-	{	if  (len != 1)
+	{
+		if  (len != 1)
 			(*head)->next->prev = NULL;
 		*head = (*head)->next;
 		return (1);
 	}
-
 	if (index  == len)
 	{
 		start->prev->next = NULL;
 		return (1);
 	}
-
 	while (current != NULL)
 	{
-		if (i == index){
+		if (i == index)
+		{
 			previous->next = current->next;
 			current->next->prev = previous;
 			return (1);
